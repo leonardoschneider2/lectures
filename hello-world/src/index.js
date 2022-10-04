1417ed962d5990b176e97cea96fe815919ae03fc
leonardo-schneider2 <leocoelho256@gmail.com>
2022-10-04 15:12:37 -0300

    criamos o index e fizemos o uso do readline

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,3 +1,17 @@
 console.log('index.js');
 
 // rode NPM START
+
+const readline = require('readline-sync');
+
+const name = readline.question('Qual seu nome ? ');
+
+console.log(`oi ${name} !`);
+
+const age = readline.question('Qual sua idade ? ');
+
+if (age >= 60) {
+  console.log(`CARALHO! ${age} anos é velho pra porra !`);
+} else {
+  console.log(`Como eu sempre digo, nunca confie em jovens de apenas ${age} anos !`);
+}
