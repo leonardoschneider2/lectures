2da30aebb9aa15f46856364b6d911b15098e8ad7
leonardo-schneider2 <leocoelho256@gmail.com>
2022-10-04 19:16:11 -0300

    Lectures 22.2 - Aula ao vivo assistida e código feito

--- aula-ao-vivo/src/app.js
+++ aula-ao-vivo/src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  // const { id } = req.params;
+  res.status(200).json({ message: 'Chegou na rota \'/\'. ' });
+});
 
 module.exports = app;
--- aula-ao-vivo/src/server.js
+++ aula-ao-vivo/src/server.js
@@ -4,7 +4,3 @@ app.listen(3001, console.log('App is running in port 3001'));
 
 // docker stop $(docker ps -qa) - PARA TODOS OS CONTAINERS
 // killall node - PARA TODOS OS CÓDIGOS NODE
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Chegou na rota \'/\'. ' });
-});
